f1e8ef71745851bd9bb627e888fa5136d0e80f91
David Winterbottom <david.winterbottom@gmail.com>
2022-10-01 22:33:02 +0100

    Update external tests for Bagels
    Which have gone up in price since I started this project!

--- test_scraping.py
+++ test_scraping.py
@@ -38,7 +38,7 @@ class TestFetchOcadoPrice:
         "product_id, price",
         (
             ("13175011", 500),
-            ("23476011", 175),
+            ("23476011", 180),
         ),
         ids=(
             "500g of Lurpak",
