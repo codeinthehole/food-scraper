0c06cdba535e5e51d24fc1dad766c8ac7e625ec7
David Winterbottom <david.winterbottom@gmail.com>
2022-11-01 22:13:15 +0000

    Move archive functionality into own module

---
+++ chow/archive.py
@@ -0,0 +1,45 @@
+import json
+import os
+from typing import Dict, List, TypedDict
+
+
+class PriceChange(TypedDict):
+    date: str
+    price: str
+
+
+class ProductPriceHistory(TypedDict):
+    name: str
+    prices: List[PriceChange]
+
+
+ArchiveProductMap = Dict[str, ProductPriceHistory]
+
+
+def load() -> ArchiveProductMap:
+    """
+    Return the product archive data structure.
+    """
+    # Archive is stored in a local file.
+    filepath = _filepath()
+    if not os.path.exists(filepath):
+        return {}
+
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def save(archive: ArchiveProductMap) -> None:
+    """
+    Save the product archive data structure.
+    """
+    filepath = _filepath()
+    with open(filepath, "w") as f:
+        return json.dump(archive, f, indent=4)
+
+
+def _filepath() -> str:
+    """
+    Return the filepath of the archive file.
+    """
+    return os.path.join(os.path.dirname(os.path.dirname(__file__)), "prices.json")
