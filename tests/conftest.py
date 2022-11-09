c85f9f862408b4d9e357c5004391d8f6de2e763e
David Winterbottom <david.winterbottom@gmail.com>
2022-11-09 12:30:01 +0000

    Extract a `fixture_filepath` fixture
    As we will want to use this on its own to move files around.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,35 @@ import pytest
 
 
 @pytest.fixture
-def fixture() -> Callable[[str], str]:
-    def _load_fixture(relative_filepath: str):
+def fixture_path() -> Callable[[str], str]:
+    """
+    Inject a function that looks up fixture filepaths.
+    """
+
+    def _path(relative_filepath: str):
         """
-        Return the contents of a fixture file.
+        Return the filepath to a fixture file.
         """
         filepath = os.path.join(
             os.path.dirname(__file__), "fixtures", relative_filepath
         )
         assert os.path.exists(filepath), f"Fixture {filepath} doesn't exist"
+        return filepath
 
-        with open(filepath, "r") as f:
+    return _path
+
+
+@pytest.fixture
+def fixture(fixture_path) -> Callable[[str], str]:
+    """
+    Inject a function that loads fixture file contents.
+    """
+
+    def _load_fixture(relative_filepath: str):
+        """
+        Return the contents of a fixture file.
+        """
+        with open(fixture_path(relative_filepath), "r") as f:
             return f.read()
 
     return _load_fixture
