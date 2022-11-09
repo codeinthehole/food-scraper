d8b4aee8886362ba287c69fa07422daf03eaa954
David Winterbottom <david.winterbottom@gmail.com>
2022-11-09 12:53:55 +0000

    Add use case for building a product overview doc

---
+++ chow/usecases/overview.py
@@ -0,0 +1,28 @@
+import pathlib
+
+from chow import archive, logger
+
+
+def generate_overview_file(
+    archive_filepath: pathlib.Path,
+    charts_folder: pathlib.Path,
+    overview_filepath: pathlib.Path,
+    logger: logger.ConsoleLogger,
+) -> None:
+    """
+    Generate an overview markdown file.
+    """
+    products_data = archive.load(str(archive_filepath))
+
+    with overview_filepath.open("w") as f:
+        f.write("# Product price charts\n")
+
+        for product_id, product_data in products_data.items():
+            image_file = charts_folder / f"product-{product_id}.png"
+            if not image_file.exists():
+                continue
+            image_url = image_file.relative_to(overview_filepath.parent)
+            line = "![{name}]({image_url})\n".format(
+                name=product_data["name"], image_url=str(image_url)
+            )
+            f.write(line)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import pathlib
 from typing import TextIO
 
 import click
@@ -38,6 +39,33 @@ def generate_graphs(archive: str, folder: str) -> None:
     usecases.generate_product_graphs(
         archive_filepath=archive,
         chart_folder=folder,
+        logger=logger.ConsoleLogger(debug_mode=True),
+    )
+
+
+@cli.command()
+@click.argument("archive")
+@click.argument("folder")
+@click.argument("overview_file")
+def generate_overview(archive, folder: str, overview_file: str) -> None:
+    """
+    Generate a product overview in the passed file.
+    """
+    # Check archive file exists
+    archive_filepath = pathlib.Path(archive)
+    assert archive_filepath.is_file() and archive_filepath.exists()
+
+    # Check charts folder exists
+    charts_folder = pathlib.Path(folder)
+    assert charts_folder.is_dir() and charts_folder.exists()
+
+    # Overview doc doesn't necessarily exist.
+    overview_filepath = pathlib.Path(overview_file)
+
+    usecases.generate_overview_file(
+        archive_filepath=archive_filepath,
+        charts_folder=charts_folder,
+        overview_filepath=overview_filepath,
         logger=logger.ConsoleLogger(debug_mode=True),
     )
 
