8cdd7764db9fdb12f3df2edbeaca5a3f06483c06
David Winterbottom <david.winterbottom@gmail.com>
2022-11-11 16:54:40 +0000

    Use JSON-Schema to validate archive file format

--- chow/archive.py
+++ chow/archive.py
@@ -2,6 +2,8 @@ import json
 import os
 from typing import Dict, List, TypedDict
 
+import jsonschema
+
 
 class PriceChange(TypedDict):
     date: str
@@ -16,6 +18,44 @@ class ProductPriceHistory(TypedDict):
 ArchiveProductMap = Dict[str, ProductPriceHistory]
 
 
+class InvalidJSON(Exception):
+    pass
+
+
+ARCHIVE_SCHEMA = {
+    "type": "object",
+    "patternProperties": {
+        r"^\d+$": {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string",
+                },
+                "prices": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "date": {
+                                "type": "string",
+                            },
+                            "price": {
+                                "type": "string",
+                            },
+                        },
+                        "required": ["date", "price"],
+                        "additionalProperties": False,
+                    },
+                },
+            },
+            "required": ["name", "prices"],
+            "additionalProperties": False,
+        }
+    },
+    "additionalProperties": False,
+}
+
+
 def load(filepath: str) -> ArchiveProductMap:
     """
     Return the product archive data structure.
@@ -24,8 +64,20 @@ def load(filepath: str) -> ArchiveProductMap:
     if not os.path.exists(filepath):
         return {}
 
+    # Decode file content.
     with open(filepath, "r") as f:
-        return json.load(f)
+        try:
+            content = json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            raise InvalidJSON("JSON could not be decoded") from e
+
+    # Validate against schema.
+    try:
+        jsonschema.validate(instance=content, schema=ARCHIVE_SCHEMA)
+    except jsonschema.exceptions.ValidationError as e:
+        raise InvalidJSON("JSON does not conform to schema") from e
+
+    return content
 
 
 def save(filepath: str, archive: ArchiveProductMap) -> None:
---
+++ tests/unit/chow/test_archive.py
@@ -0,0 +1,94 @@
+import json
+
+import pytest
+
+from chow import archive
+
+
+class TestLoad:
+    def test_no_file(self):
+        assert archive.load("/tmp/does-not-exist") == {}
+
+    def test_empty_file(self, tmp_path):
+        # Create file with empty dict content.
+        archive_file = tmp_path / "prices.json"
+        archive_file.write_text(json.dumps({}))
+
+        assert archive.load(str(archive_file)) == {}
+
+    def test_valid_file(self, tmp_path):
+        # Create file with empty dict content.
+        archive_file = tmp_path / "prices.json"
+        content = {
+            "123": {
+                "name": "X",
+                "prices": [
+                    {"date": "2022-10-02", "price": "5.00"},
+                ],
+            },
+        }
+        archive_file.write_text(json.dumps(content))
+
+        assert archive.load(str(archive_file)) == content
+
+    def test_missing_prices(self, tmp_path):
+        # Create file with empty dict content.
+        archive_file = tmp_path / "prices.json"
+        content = {
+            "123": {
+                "name": "X",
+            },
+        }
+        archive_file.write_text(json.dumps(content))
+
+        with pytest.raises(archive.InvalidJSON):
+            archive.load(str(archive_file)) == content
+
+    def test_misspelt_name(self, tmp_path):
+        # Create file with empty dict content.
+        archive_file = tmp_path / "prices.json"
+        content = {
+            "123": {
+                "mane": "X",
+                "prices": [
+                    {"date": "2022-10-02", "price": "5.00"},
+                ],
+            },
+        }
+        archive_file.write_text(json.dumps(content))
+
+        with pytest.raises(archive.InvalidJSON):
+            archive.load(str(archive_file)) == content
+
+    def test_invalid_product_id(self, tmp_path):
+        # Create file with empty dict content.
+        archive_file = tmp_path / "prices.json"
+        content = {
+            "123x": {
+                "name": "X",
+                "prices": [
+                    {"date": "2022-10-02", "price": "5.00"},
+                ],
+            },
+        }
+        archive_file.write_text(json.dumps(content))
+
+        with pytest.raises(archive.InvalidJSON):
+            archive.load(str(archive_file)) == content
+
+    def test_extra_properties(self, tmp_path):
+        # Create file with empty dict content.
+        archive_file = tmp_path / "prices.json"
+        content = {
+            "123": {
+                "name": "X",
+                "egg": "X",
+                "prices": [
+                    {"date": "2022-10-02", "price": "5.00"},
+                ],
+            },
+        }
+        archive_file.write_text(json.dumps(content))
+
+        with pytest.raises(archive.InvalidJSON):
+            archive.load(str(archive_file)) == content
